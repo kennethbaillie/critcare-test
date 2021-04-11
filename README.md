76894137810e092368168dbae7bd27972f4a4efd
Kenneth Baillie <github@baillielab.net>
2021-04-11 13:19:33 +0100

    make script to download directory tree - completely replaces online directory every time

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
 # critcare.github.io
 NHS Lothian Critical Care Guidelines
 
-## TODO
-
-- improve search - maybe solr rather than lunr?
-- add "add to home screen" button
-- make downloadable
-- tidy up css on home screen to make it easier to enter on mobile
-
-
 ## Website updates
 
 These steps occur to update the website. They are specified in yml files in the github "actions" tab:
@@ -16,10 +8,18 @@ These steps occur to update the website. They are specified in yml files in the 
 - makelist - this makes a directory tree into an html menu, and makes a search index for pdfs
 - mkdocs - this runs mkdocs to create the live online branch
 
-
 ## To change password
 The directory name has to change in the following places:
 - .github/workflows/makelist.yml
 - the actual dir itself
 - the index file
+
+## TODO
+
+- improve search - maybe solr rather than lunr?
+- add "add to home screen" button
+- make downloadable
+- tidy up css on home screen to make it easier to enter on mobile
+- speed up file copying by checking for identical files/more recently updated files and avoiding copying them
+- speed up search indexing by storing extracted data from pdfs somewhere and only updating if file is changed/added/deleted
 
--- download.py
+++ download.py
@@ -3,10 +3,9 @@
 import os
 import sys
 import json
-import requests
 import shutil
+import requests
 import urllib.parse
-
 
 scriptpath = os.path.dirname(os.path.realpath(__file__))
 #-----------------------------
@@ -23,7 +22,6 @@ def get_document_urls_from_url(folder_url):
     line = [l for l in html.split("\n") if "prefetch-shared_link_folder_entries-ShmodelFolderEntriesPrefetch-1" in l][0]
     json_data = line.split("responseReceived(")[1].replace(")});", "").replace('\\', '"').replace('""', '"')[1:-1]
     data = json.loads(json_data)
-    print (data)
     return [link["url"] for link in data["shared_link_infos"]]
 
 def download_files_from_dir(dir_name, folder_url):
@@ -31,14 +29,28 @@ def download_files_from_dir(dir_name, folder_url):
     for url in urls:
         base_url = url.split('?')[0]
         filename = urllib.parse.unquote(base_url).split('/')[-1]
-        file_url = base_url + '?dl=1'
-        print("Downloading file:", file_url)
-        r = requests.get(file_url)
+        destinationfile = os.path.join(dir_name, filename)
         if not os.path.exists(dir_name):
             os.makedirs(dir_name, exist_ok=True)
-        with open(os.path.join(dir_name, filename), 'wb') as f:
-            f.write(r.content)
+        if "." in filename:
+            file_url = base_url + '?dl=1'
+            print("Downloading file:", file_url)
+            r = requests.get(file_url)
+            with open(destinationfile, 'wb') as f:
+                f.write(r.content)
+        else:
+            # then this is a directory. Download the whole shebang
+            destinationdir = os.path.join(dir_name, filename)
+            print ("Downloading subdirectory:", filename)
+            download_files_from_dir(destinationdir, url)
 
 if os.path.exists(args.destinationdir):
     shutil.rmtree(args.destinationdir)
-download_files_from_dir(args.destinationdir, subdir[1])
+download_files_from_dir(args.destinationdir, args.sourcedir)
+
+
+
+
+
+
+
