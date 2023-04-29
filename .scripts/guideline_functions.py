71c49245ccf3d8b18fff45fe8f0d3e973284827e
Kenneth Baillie <github@baillielab.net>
2023-04-29 19:51:26 +0100

    ditch pypdf2

--- .scripts/guideline_functions.py
+++ .scripts/guideline_functions.py
@@ -13,7 +13,6 @@ from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
-import PyPDF2
 
 #-----------------------------
 # files to ignore
@@ -104,12 +103,15 @@ def readfilecontents(thisfile):
             return ""
 
 def get_pdf_text(file_path):
+    return readfilecontents(file_path)
+    '''
     with open(file_path, 'rb') as f:
         pdf_reader = PyPDF2.PdfFileReader(f, strict=False)
         text = ''
         for page_num in range(pdf_reader.numPages):
             text += pdf_reader.getPage(page_num).extractText()
         return text
+    '''
 
 
 def recursive_split(s):
